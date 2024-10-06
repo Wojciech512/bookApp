8021b7339c0d049d44223649e09cfae302451dbe
Wojtek Wyleżoł <w.wylezol@wp.pl>
2024-10-06 21:34:44 +0200

    Init commit

--- backend/DRF_ANGULAR_APP_BACKEND/books/models.py
+++ backend/DRF_ANGULAR_APP_BACKEND/books/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Book(models.Model):
     CATEGORY_CHOICES = (
-        ('Thriller','Thriller'),
-        ('Fantasy','Fantasy'),
-        ('Novel','Novel'),
-
+        ("Thriller", "Thriller"),
+        ("Fantasy", "Fantasy"),
+        ("Novel", "Novel"),
     )
 
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    category = models.CharField(max_length=50 , choices=CATEGORY_CHOICES)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, blank=True)
 
     def __str__(self):
         return self.title
